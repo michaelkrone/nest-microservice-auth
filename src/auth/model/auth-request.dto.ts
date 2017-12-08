dd375287eb6e5ae07283c90551278f6009075cf5
michaelkrone <michael.krone@outlook.com>
2017-12-08 23:04:54 +0100

    docs(swagger)

--- src/auth/model/auth-request.dto.ts
+++ src/auth/model/auth-request.dto.ts
@@ -1,9 +1,16 @@
+import { ApiModelProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class AuthRequestDto {
-	@IsString() readonly id: string;
+	@ApiModelProperty()
+	@IsString()
+	readonly id: string;
 
-	@IsString() readonly password: string;
+	@ApiModelProperty()
+	@IsString()
+	readonly password: string;
 
-	@IsNotEmpty() readonly payload: any;
+	@ApiModelProperty()
+	@IsNotEmpty()
+	readonly payload: any;
 }
